842aa065739df3a89958a809b771682286dfad8b
caio souza <caiocbahia@hotmail.com>
2022-04-02 23:11:19 -0300

    lifecycle hooks in action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test'}];
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test'},];
 
   onServerAdded(serverData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
@@ -22,6 +22,14 @@ export class AppComponent {
       name: blueprintData.serverName,
       content: blueprintData.serverContent
     });
+  }
+
+  onChangeFirst(){
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst(){
+    this.serverElements.splice(0,1);
   }
 
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { compileNgModule } from '@angular/compiler';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -8,12 +22,53 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 })
 
 
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+OnInit,
+OnChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called!')
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called!')
+    console.log(changes)
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!')
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called!')
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called!')
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!')
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called!')
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!')
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!')
   }
 
 }
